2ea0081be91909bd8e917256e4601aa3b56e6a4b
Tejas Vinayaka Rao Kangod <kangod@usc.edu>
2025-05-29 16:36:38 -0700

    gcloud deploy

--- Server/server.js
+++ Server/server.js
@@ -1,5 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
 
 import checkExistingUserRoute from "./routes/queryRoutes/checkExistingUser.js";
 import getSongs from "./routes/queryRoutes/getSongs.js";
@@ -17,6 +19,11 @@ const app = express();
 dotenv.config();
 app.use(express.json());
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const buildFilePath = path.join(__dirname, "build");
+app.use(express.static(buildFilePath));
+
 app.listen(process.env.PORT, () => {
   console.log("Server started successfully", process.env.PORT);
 });
@@ -31,3 +38,7 @@ app.use(deletePlaylist);
 app.use(updateHomepageSong);
 app.use(updatePlaylistSong);
 app.use(updatePlaylistId);
+
+app.get(/^\/(?!api).*/, (req, res) => {
+  res.sendFile(path.join(buildFilePath, "index.html"));
+});

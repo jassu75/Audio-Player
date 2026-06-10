143110c4084fab1026747978b82ce732d8fc061c
Tejas Vinayaka Rao Kangod <kangod@usc.edu>
2026-06-10 11:21:43 -0700

    moveSong

--- client/src/components/playlist/actions/song/moveSong/MoveSong.jsx
+++ client/src/components/playlist/actions/song/moveSong/MoveSong.jsx
@@ -16,48 +16,48 @@ import usePlaylistMetadata from "../../../../../pages/playlist/hooks/usePlaylist
 import { getUserPlaylistRailCover } from "../../../../playlisRail/utils/playlistRail.utils";
 
 const PlaylistSelector = ({ filteredPlaylists, selected, handleSelect }) => {
+  if (!filteredPlaylists || filteredPlaylists.length === 0) {
+    return <Typography>No playlists available</Typography>;
+  }
+
   return (
     <Grid2 className={styles.playlist_container}>
-      {filteredPlaylists.length > 0 ? (
-        filteredPlaylists.map(([id, playlist]) => {
-          const playlistCover = getUserPlaylistRailCover(
-            playlist?.playlist_cover_art,
-          );
+      {filteredPlaylists.map((playlist) => {
+        const playlistCover = getUserPlaylistRailCover(
+          playlist?.playlist_cover_art,
+        );
 
-          return (
-            <ButtonBase
-              key={id}
-              className={`${styles.playlist_item} ${
-                selected === id ? styles.selected_item : ""
-              }`}
-              onClick={() => handleSelect(id)}
-            >
-              <Radio
-                checked={selected === id}
-                onChange={() => handleSelect(id)}
-              />
+        return (
+          <ButtonBase
+            key={playlist?.playlist_id}
+            className={`${styles.playlist_item} ${
+              selected === playlist?.playlist_id ? styles.selected_item : ""
+            }`}
+            onClick={() => handleSelect(playlist?.playlist_id)}
+          >
+            <Radio
+              checked={selected === playlist?.playlist_id}
+              onChange={() => handleSelect(playlist?.playlist_id)}
+            />
 
-              <img
-                loading="lazy"
-                alt=""
-                src={playlistCover}
-                className={styles.playlist_image}
-              />
+            <img
+              loading="lazy"
+              alt=""
+              src={playlistCover}
+              className={styles.playlist_image}
+            />
 
-              <Grid2 className={styles.playlist_content}>
-                <Typography
-                  variant="RedirectText"
-                  className={styles.playlist_title}
-                >
-                  {playlist?.playlist_title}
-                </Typography>
-              </Grid2>
-            </ButtonBase>
-          );
-        })
-      ) : (
-        <Typography>No playlists available</Typography>
-      )}
+            <Grid2 className={styles.playlist_content}>
+              <Typography
+                variant="RedirectText"
+                className={styles.playlist_title}
+              >
+                {playlist?.playlist_title}
+              </Typography>
+            </Grid2>
+          </ButtonBase>
+        );
+      })}
     </Grid2>
   );
 };
